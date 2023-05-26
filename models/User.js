c9dc8a539558e564d24e1eb6cf9eaefb13889c80
Felipe Farnetti <Felipefarnetti@gmail.com>
2023-05-26 12:38:04 +0200

    routes working

--- models/User.js
+++ models/User.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const User = mongoose.model("user", {
   email: { type: String, required: true },
   username: { type: String, required: true },
-  token: { type: String },
-  salt: { type: String },
-  hash: { type: String },
+  token: { type: String, required: true },
+  salt: { type: String, required: true },
+  hash: { type: String, required: true },
   favorites: [{ type: String }],
 });
 
